d27cfde05dd72ed2889407f0904667f7fa5d369e
NTLam108 <lam.nt108204@gmail.com>
2025-10-21 14:31:18 +0700

    update

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { Express } from "express"
-import { getCreateUserpage, getHomepage, postCreateUserpage } from "../controllers/user.controller";
+import { getCreateUserpage, getHomepage, getViewUser, postCreateUserpage, postDeleteUser, postUpdateUser } from "controllers/user.controller";
 const router = express.Router();
 
 const webRoutes = (app: Express) => {
     router.get("/", getHomepage)
     router.get("/create-user", getCreateUserpage)
     router.post("/handle-create-user", postCreateUserpage)
+    router.post("/handle-delete-user/:id", postDeleteUser)
+    router.get("/handle-view-user/:id", getViewUser)
+    router.post("/handle-update-user", postUpdateUser)
     app.use("/", router)   // "/" == website bay dau tu duong dan nao
 }
 
