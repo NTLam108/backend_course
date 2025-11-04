4e41d6d3865c85a7157d54bdefd7414858bd9bb0
NTLam108 <lam.nt108204@gmail.com>
2025-11-04 22:07:00 +0700

    update

--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -37,4 +37,40 @@ const handleDeleteCar = async (id: string) => {
     })
 }
 
-export { handleCreateCar, getAllCars, handleDeleteCar }
+const getCarDetailbyId = async (id: string) => {
+    const car = await prisma.car.findUnique({
+        where: { id: +id }
+    })
+    return car;
+}
+
+const updateCarbyId = async (
+    id: string,
+    name: string,
+    priceperday: number,
+    brand: string,
+    carType: string,
+    seat: number,
+    engine: string,
+    status: string,
+    carImage: string,
+    detailCar: string,
+) => {
+    const updateCar = await prisma.car.update({
+        where: { id: +id },
+        data: {
+            name: name,
+            priceperday: priceperday,
+            brand: brand,
+            carType: carType,
+            seat: seat,
+            engine: engine,
+            status: status,
+            ...(carImage !== undefined && { carImage: carImage }),
+            detailCar: detailCar
+        },
+    });
+    return updateCar
+}
+
+export { handleCreateCar, getAllCars, handleDeleteCar, getCarDetailbyId, updateCarbyId }
